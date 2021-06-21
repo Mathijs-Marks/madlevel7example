0561575b97e5bd9bd97da08377bef1fed5a6422f
Mathijs Marks <mathijsmarks@msn.com>
2021-06-21 22:35:27 +0200

    Created Quiz fragment and setup navigation.

---
+++ app/src/main/java/com/example/madlevel7example/QuizFragment.kt
@@ -0,0 +1,32 @@
+package com.example.madlevel7example
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.madlevel7example.databinding.FragmentQuizBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [QuizFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class QuizFragment : Fragment() {
+
+    private var _binding: FragmentQuizBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentQuizBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

b68eacb7579bf86f3f36a2ce80cf804a4c8ff928
Mathijs Marks <mathijsmarks@msn.com>
2021-06-21 22:59:49 +0200

    Setup Firestore, Model, ViewModel, and Repository.

---
+++ app/src/main/java/com/example/madlevel7example/QuizRepository.kt
@@ -0,0 +1,62 @@
+package com.example.madlevel7example
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withTimeout
+
+class QuizRepository {
+
+    private var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private var quizDocument =
+        firestore.collection("quizzes").document("quiz")
+
+    private val _quiz: MutableLiveData<Quiz> = MutableLiveData()
+
+    val quiz: LiveData<Quiz>
+        get() = _quiz
+
+    // The CreateQuizFragment can use this to see if the creation succeeded.
+    private val _createSuccess: MutableLiveData<Boolean> = MutableLiveData()
+
+    val createSuccess: LiveData<Boolean>
+        get() = _createSuccess
+
+    suspend fun getQuiz() {
+        try {
+            // Firestore has support for coroutines via the extra dependency we've added :)
+            withTimeout(5_000) {
+                val data = quizDocument
+                    .get()
+                    .await()
+
+                val question = data.getString("question").toString()
+                val answer = data.getString("answer").toString()
+
+                _quiz.value = Quiz(question, answer)
+            }
+        } catch (e: Exception) {
+            throw QuizRetrievalError("Retrieval-firebase-task was unsuccessful")
+        }
+    }
+
+    suspend fun createQuiz(quiz: Quiz) {
+        // Persist data to Firestore.
+        try {
+            // Firestore has support for coroutines via the extra dependency we've added :)
+            withTimeout(5_000) {
+                quizDocument
+                    .set(quiz)
+                    .await()
+
+                _createSuccess.value = true
+            }
+        } catch (e: Exception) {
+            throw QuizSaveError(e.message.toString(), e)
+        }
+    }
+
+    class QuizSaveError(message: String, cause: Throwable) : Exception(message, cause)
+    class QuizRetrievalError(message: String) : Exception(message)
+}
